9e4e73f6d19ae70ea98026284a9a0f898e06c1c3
MageLink <mage.link.zhezard@gmail.com>
2023-04-03 18:10:52 -0400

    Starting point

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Given App component", () => {
+  test("It should exist", () => {
+    const test = render(<App />);
+    console.log("Test", test);
+  });
 });
